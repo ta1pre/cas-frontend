f2f4ea2dbc5ada49828efc2e71e1f318006fa5f3
ta1pre <taichiumeki@gmail.com>
2025-03-26 00:53:48 +0900

    お客様向けオファーページの実装: Next.jsのページコンポーネントとして作成

---
+++ src/app/p/customer/offer/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function CustomerOfferPage() {
+  return (
+    <div>
+      <h1>オファーページ</h1>
+      <p>ここはお客様向けのオファーページです</p>
+    </div>
+  );
+}
